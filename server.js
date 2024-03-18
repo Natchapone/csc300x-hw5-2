9961542c8184a1955f3393f7988c2b70e36fea06
Natchapone Janjon <n_janjon@uncg.edu>
2024-03-17 22:19:56 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function () {
-    console.log('app listening on port: ' + PORT + "!");
+
+let categories = ['funnyJoke', 'lameJoke'];
+let funnyJoke = [
+    {
+        'joke': 'Why did the student eat his homework?',
+        'response': 'Because the teacher told him it was a piece of cake!'
+    },
+    {
+        'joke': 'What kind of tree fits in your hand?',
+        'response': 'A palm tree'
+    },
+    {
+        'joke': 'What is worse than raining cats and dogs?',
+        'response': 'Hailing taxis'
+    }
+];
+let lameJoke = [
+    {
+        'joke': 'Which bear is the most condescending?',
+        'response': 'Pan-DUH'
+    },
+    {
+        'joke': 'What would the Terminator be called in his retirement?',
+        'response': 'The Exterminator'
+    }
+];
+
+//Categories endpoint
+app.get('/jokebook/categories', (req, res) => {
+    res.json(categories);
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server listening to port ${PORT}`);
 });
